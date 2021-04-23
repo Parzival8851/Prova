0aa32797afe76f849c642793cb91462f0d88379e
unknown <claudia.agosti01@gmail.com>
2021-04-23 17:02:26 +0200

    prima modifica

--- src/Test.java
+++ src/Test.java
@@ -1,8 +1,8 @@
 public class Test
 {
     public Test() {
-        System.out.println("Ciao da Bonio!\n");
-
+        System.out.println("Ciao da Bonio! ciaoooooo, mi è passato il torcicollo\n");
+        System.out.println("voglio tornare in uni");
 
     }
 }
